d9441ad8fd2b2952c5989013f50db53677e1c609
xiaovzhi <freesurfing@gmail.com>
2017-07-14 19:27:18 +0800

    add page num calc，don't pass a parameter

--- Crawler/fetch_poetry.py
+++ Crawler/fetch_poetry.py
@@ -9,18 +9,10 @@ import re
 import time
 import requests
 
-# import jieba
-# import jieba.posseg as pseg
-# import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# from os import path
 from collections import Counter
-# from scipy.misc import imread
-# from wordcloud import WordCloud
 
-class ShiCi:  
+
+class Poem:  
     name = ''  
     title = ''  
     content = ''
@@ -30,38 +22,47 @@ class ShiCi:
         self.title = t  
         self.content = c  
 
-def fetch_shici(baseurl, keyword, pagenum, sclist, outfile):
-    PATTERN  = re.compile('<TR><TD>(.*?)</TD><TD>(.*?)</TD><TD>(.*?)</TD></TR>')
-    BASE_URL = baseurl
-    MAX_PAGE_NUM = pagenum+1
-    
+def fetch_poetry(baseurl, keyword, poemlist, outfile):
+    base_url = baseurl
     bytes_key = keyword.encode('gbk')
     input_str = '%{0:2X}%{1:2X}'.format(bytes_key[0], bytes_key[1])
 
+    # in fisrt page, calc total page num
+    page_pattern = re.compile('<A HREF=\'(.*?)\'>(.*?)</A>&nbsp;&nbsp;')
+    r = requests.get(base_url + str(1) +'&tp=3&inputword=' + input_str)
+    r.encoding='gbk'
+    data  = r.text
+    page_info = re.findall(page_pattern, data)
+    page_num = len(page_info)
+    print(page_num)
+
+    # find contain keyword poem
+    max_page_num = page_num+1
+    poem_pattern = re.compile('<TR><TD>(.*?)</TD><TD>(.*?)</TD><TD>(.*?)</TD></TR>')    
     with open(outfile, 'w', encoding='utf-8') as f:
-        for i in range(1, MAX_PAGE_NUM):
+        for i in range(1, max_page_num):
             print('Downloading page #{}'.format(i))
-            r = requests.get(BASE_URL + str(i) +'&tp=3&inputword=' + input_str)
+            r = requests.get(base_url + str(i) +'&tp=3&inputword=' + input_str)
             r.encoding='gbk'
             data = r.text
-            p = re.findall(PATTERN, data)
+            p = re.findall(poem_pattern, data)
             for s in p:
-                sc = ShiCi('','','')
+                sc = Poem('','','')
                 sc.name    = s[0]
                 sc.title   = s[1]
                 sc.content = "".join(s[2].split()); #去除回车符
-                sclist.append(sc)
+                poemlist.append(sc)
                 wstr = s[0]+'\t\t'+s[1]+'\t\t\t\t'+s[2]+'\n'
                 f.write(wstr)
             time.sleep(2)
 
-def analysis_sc(sclist, outfile, cntfile):
+def analysis_sc(poemlist, outfile, cntfile):
     nlist=[]
-    sclist.sort(key=lambda sc : sc.name)
-    print(len(sclist))
+    poemlist.sort(key=lambda sc : sc.name)
+    print(len(poemlist))
     
     with open(outfile, 'w', encoding='utf-8') as f:    
-        for sc in sclist:
+        for sc in poemlist:
             # print(sc.name)
             nlist.append(sc.name)
             wstr = '{0:s}\t\t{1:s}\t\t{2:s}\n'.format(sc.name, sc.title, sc.content)
@@ -75,27 +76,27 @@ def analysis_sc(sclist, outfile, cntfile):
             cstr='{0:s}\t{1:d}\n'.format(name, freq)
             f.write(cstr)
 
+def search_word(word):
+    keyword = word
+
+    poemlist=[]
+    f1='唐诗_'+keyword+'.txt'
+    f2='唐诗_'+keyword+'_s.txt'
+    f3='唐诗_'+keyword+'_c.txt'    
+    baseurl = "http://www.poeming.com/web/qtssearch.asp?Page="
+    fetch_poetry(baseurl, keyword, poemlist, f1)    
+    analysis_sc(poemlist, f2, f3)
+
+    poemlist=[]
+    f1='宋词_'+keyword+'.txt'
+    f2='宋词_'+keyword+'_s.txt'
+    f3='宋词_'+keyword+'_c.txt'        
+    baseurl = "http://www.poeming.com/web/qscsearch.asp?Page="
+    fetch_poetry(baseurl, keyword, poemlist, f1)
+    analysis_sc(poemlist, f2, f3)    
+
 if __name__ == "__main__":   
-    keyword = '菊'
+    search_word('菊')
+    search_word('志')
 
-    sclist=[]
-    baseurl = "http://www.poeming.com/web/qtssearch.asp?Page="
-    fetch_shici(baseurl, keyword, 23, sclist, 'tanshi_ju.txt')
-    analysis_sc(sclist, 'tanshi_ju_sort.txt', 'count_ts_ju.txt')
-
-    sclist=[]
-    baseurl = "http://www.poeming.com/web/qscsearch.asp?Page="
-    fetch_shici(baseurl, keyword, 23, sclist, 'songci_ju.txt')
-    analysis_sc(sclist, 'songci_ju_sort.txt', 'count_sc_ju.txt')    
-
-    keyword = '志'
-
-    sclist=[]
-    baseurl = "http://www.poeming.com/web/qtssearch.asp?Page="
-    fetch_shici(baseurl, keyword, 25, sclist, 'tanshi_zhi.txt')
-    analysis_sc(sclist, 'tanshi_zhi_sort.txt', 'count_ts_zhi.txt')
-
-    sclist=[]
-    baseurl = "http://www.poeming.com/web/qscsearch.asp?Page="
-    fetch_shici(baseurl, keyword, 7, sclist, 'songci_zhi.txt')
-    analysis_sc(sclist, 'songci_zhi_sort.txt', 'count_sc_zhi.txt')        
+    search_word('风')
